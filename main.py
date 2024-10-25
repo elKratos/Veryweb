803c35ea40ec2f3d76ea5d757da42dd8274bc9b5
Moises <elkratos42@gmail.com>
2024-10-25 16:56:54 -0400

    Add download and hasing

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ import difflib
 import os
 import hashlib
 
+# TODO: Realiza una aplicación que compruebe cada 30 segundos si hay algún cambio en una página web. Si existe el cambio, muestra las diferencias y para su ejecución.
+# TODO: Puedes mejorar este programa enviando las diferencias por correo electrónico.
+
 class main():
-    pass
+
+    # TODO: Descargar el codigo de la web
+    def download_web(url): pass
+
+    # TODO: Sacar hash de la web descargada
+    def hashing(code, hash): pass
