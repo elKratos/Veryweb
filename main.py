13c6966707d9b87f80c5ad9f4247efeabdfc03e9
Moises <elkratos42@gmail.com>
2024-10-25 17:53:04 -0400

    Implement a wating and hashing methods

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ import hashlib
 # TODO: Realiza una aplicación que compruebe cada 30 segundos si hay algún cambio en una página web. Si existe el cambio, muestra las diferencias y para su ejecución.
 # TODO: Puedes mejorar este programa enviando las diferencias por correo electrónico.
 
-# TODO: Esperamos 30 segundos
-def witing(): pass
+# Esperamos 30 segundos
+def witing(): time.sleep(30)
 
 # TODO: Descargar el codigo de la web
 def download_web(url): pass
 
-# TODO: Sacar hash de la web descargada
-def hashing(code, hash): pass
+# Obtener el hash del archivo
+def hashing(file_path): 
+    file = open(file_path, 'rb')
+    return hashlib.sha256(file.read()).hexdigest()
 
 # TODO: El comparar hash. 
 #       Tenemos un hash guardado? No, lo guardamos. Si, los comparamos.
@@ -27,3 +29,6 @@ def hashing(code, hash): pass
 # TODO: Obtenemos la diferencia y la enviamos por correo.
 
 # TODO: Detenemos la aplicacion
+    
+
+print(hashing("index.html"))
