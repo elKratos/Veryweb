abc9675edc5e0b1c084d10ab79f38c859f198b28
Moises <elkratos42@gmail.com>
2024-10-25 17:07:01 -0400

    Add TODO for orientation

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ import hashlib
 # TODO: Realiza una aplicación que compruebe cada 30 segundos si hay algún cambio en una página web. Si existe el cambio, muestra las diferencias y para su ejecución.
 # TODO: Puedes mejorar este programa enviando las diferencias por correo electrónico.
 
-class main():
+# TODO: Esperamos 30 segundos
+def witing(): pass
 
-    # TODO: Descargar el codigo de la web
-    def download_web(url): pass
+# TODO: Descargar el codigo de la web
+def download_web(url): pass
 
-    # TODO: Sacar hash de la web descargada
-    def hashing(code, hash): pass
+# TODO: Sacar hash de la web descargada
+def hashing(code, hash): pass
+
+# TODO: El comparar hash. 
+#       Tenemos un hash guardado? No, lo guardamos. Si, los comparamos.
+#       El hash es igual? Si, seguimos. No, paramos de descargar y buscamos el cambio.
+
+# TODO: Comparamos el codigo fuente y sacamos la diferencia.
+
+# TODO: Obtenemos la diferencia y la enviamos por correo.
+
+# TODO: Detenemos la aplicacion
